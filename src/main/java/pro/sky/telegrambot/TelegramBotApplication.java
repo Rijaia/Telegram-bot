efe32db32bc87a033a10b9f7ac6c05545c046930
Rijaia <zaya_89@mail.ru>
2023-09-19 12:38:20 +0300

    database connected

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -1,16 +1,16 @@
 package pro.sky.telegrambot;
 
-import org.apache.xpath.operations.String;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
 public class TelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelegramBotApplication.class, java.lang.String.valueOf(args));
+	public static void main(String[] args) {SpringApplication.run(TelegramBotApplication.class, args);
 	}
 
 }
